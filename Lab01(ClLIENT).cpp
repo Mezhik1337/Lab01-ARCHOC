5a9dc62a06817fc047e3dd35d816bb500a9391e4
Mezhik1337 <59055036+Mezhik1337@users.noreply.github.com>
2021-10-21 15:19:45 +0300

    Add files via upload

---
+++ Lab01(ClLIENT).cpp
@@ -0,0 +1,141 @@
+﻿#define _SILENCE_EXPERIMENTAL_FILESYSTEM_DEPRECATION_WARNING
+
+#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+#include <winsock2.h>
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <vector>
+#include<EXPERIMENTAL/filesystem>
+using namespace std;
+
+
+void send_file(SOCKET* sock, const string& file_name) {
+    fstream file;
+    file.open(file_name, ios_base::in | ios_base::binary);
+
+    if (file.is_open()) {
+        int file_size = experimental::filesystem::file_size(file_name) + 1;
+
+        char* bytes = new char[file_size];
+
+        file.read(bytes, file_size);
+
+        cout << "size " << file_size << endl;
+        cout << "name " << file_name << endl;
+
+        send(*sock, to_string(file_size).c_str(), 61, 0);
+        send(*sock, file_name.c_str(), 32, 0);
+        send(*sock, bytes, file_size, 0);
+
+        delete[] bytes;
+    }
+    else
+        cout << "Error file open" << endl;
+    file.close();
+}
+SOCKET Connect;
+string sent = " sent: ";
+string received = " received: ";
+void log(string message, string action)
+{
+    time_t rawtime;
+    struct tm* timeinfo;
+
+    time(&rawtime);                         // current date using seconds
+    timeinfo = localtime(&rawtime);        // current date in normal form
+
+    ofstream out;
+    out.open("C:\\Users\\38067\\Desktop\\Археос\\logfile.txt", ios::app);
+
+    if (out.is_open())
+    {
+        out << asctime(timeinfo) << "Client" << action << message << endl;
+    }
+    out.close();
+}
+
+void GetInfo()
+{
+    int size_of_info;
+    recv(Connect, (char*)&size_of_info, sizeof(size_of_info), NULL);
+    char* info = new char[size_of_info + 1];
+    info[size_of_info] = '\0';
+    recv(Connect, info, size_of_info, NULL);
+    string info_reinitialize = info;
+    log(info, received);
+    cout << info << endl;
+    string short_str = info_reinitialize.substr(0, 4);
+    int num = 0;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    WSAData wsadata;
+    WORD DLLVersion = MAKEWORD(2, 1);
+
+    if (WSAStartup(DLLVersion, &wsadata) != 0)
+    {
+        cout << "WSAStartup couldn't upload winsock version" << endl;
+        log("WSAStartup couldn't upload winsock version", " error: ");
+        exit(1);
+    }
+
+    SOCKADDR_IN addr;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(1027);
+    addr.sin_family = AF_INET;
+
+    Connect = socket(AF_INET, SOCK_STREAM, NULL);
+
+    if (connect(Connect, (SOCKADDR*)&addr, sizeof(addr)) != 0)
+    {
+        cout << "Connection failed" << endl;
+        log("Connection failed", " error: ");
+        return 1;
+    }
+    else
+    {
+        cout << "Client connected" << endl;
+        log("Client connected", " upload successful: ");
+
+        while (true)
+        {
+            cout << "Wait for command input:" << endl;
+            log("Wait for command input:", " state: ");
+            string command;
+            cin >> command;
+            if (command == "transfer") 
+            {
+                cout << "Please input file name" << endl;
+                string name;
+                cin >> name;
+                send_file(&Connect, name);
+            }
+            if (command == "correct") {
+                cout << "Enter number of line:";
+                int number;
+                cin >> number;
+                while (number < 1 || number>5) {
+                    cout << "Wrong input! Enter number from 1 to 5\n";
+                    cin >> number;
+                }
+                command = command + " " + to_string(number);
+            }
+            int size_of_command = size(command);
+            send(Connect, (char*)&size_of_command, sizeof(size_of_command), NULL);
+            send(Connect, command.c_str(), size_of_command, NULL);
+            log(command, sent);
+            GetInfo();
+            if (command == "help") {
+                cout << "who - for getting some details about author\n";
+                cout << "transfer - for transfer your file to server\n";
+            }
+        }
+    }
+    return 0;
+}
---
+++ Lab01(SERVER).cpp
@@ -0,0 +1,170 @@
+﻿#include <iostream>
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+#include <winsock2.h>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <ctype.h>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+
+
+void recv_file(SOCKET* sock) {
+    char file_size_str[16];
+    char file_name[32];
+
+    recv(*sock, file_size_str, 16, 0);
+    int file_size = atoi(file_size_str);
+    char* bytes = new char[file_size];
+
+    recv(*sock, file_name, 32, 0);
+
+    fstream file;
+    file.open(file_name, ios_base::out | ios_base::binary);
+
+    cout << "size " << file_size << endl;
+    cout << "name " << file_name << endl;
+
+    if (file.is_open()) {
+        recv(*sock, bytes, file_size, 0);
+        cout << "data: " << bytes << endl;
+
+        file.write(bytes, file_size);
+        cout << "Save!" << endl;
+    }
+    else
+        cout << "Error file open" << endl;
+
+    delete[] bytes;
+    file.close();
+
+
+}
+
+vector<SOCKET> connections;
+int connections_counter = -1;
+string sent = " sent: ";
+string received = " received: ";
+void log(string message, string action)
+{
+    time_t rawtime;
+    struct tm* timeinfo;
+
+    time(&rawtime);                         // текущая дата, выраженная в секундах
+    timeinfo = localtime(&rawtime);        // текущая дата, представленная в нормальной форме
+
+    ofstream out;
+    out.open("C:\\Users\\38067\\Desktop\\Археос\\logfile.txt", ios::app);
+
+    if (out.is_open())
+    {
+        out << asctime(timeinfo) << "Server" << action << message << endl;
+    }
+    out.close();
+}
+
+
+void SendInfo(int connection_marker)
+{
+    while (true)
+    {
+        int size_of_command;
+        recv(connections[connection_marker], (char*)&size_of_command, sizeof(size_of_command), NULL);
+
+        char* command = new char[size_of_command + 1];
+        command[size_of_command] = '\0';
+        if (recv(connections[connection_marker], command, size_of_command, NULL) < 0)
+        {
+            closesocket(connections[connection_marker]);
+            break;
+        }
+        string command_reinitialize = command;
+        log(command_reinitialize, received);
+        transform(begin(command_reinitialize), end(command_reinitialize), begin(command_reinitialize), tolower);
+        string short_command = command_reinitialize.substr(0, size_of_command - 2);
+        if (command_reinitialize == "who")
+        {
+            string info = "Client and server created by Mezhybovskiy Yura(file transfer)from K-27 group for variant 2";
+            int size_of_info = size(info);
+            send(connections[connection_marker], (char*)&size_of_info, sizeof(size_of_info), NULL);
+            send(connections[connection_marker], info.c_str(), size_of_info, NULL);
+            log(info, sent);
+        }
+        else if (command_reinitialize == "help")
+        {
+            string info = "info recieved";
+            int size_of_info = size(info);
+            send(connections[connection_marker], (char*)&size_of_info, sizeof(size_of_info), NULL);
+            send(connections[connection_marker], info.c_str(), size_of_info, NULL);
+            log(info, sent);
+        }
+        else
+        {
+            string error_text = "Error: unknown command";
+            int size_of_error_text = size(error_text);
+            send(connections[connection_marker], (char*)&size_of_error_text, sizeof(size_of_error_text), NULL);
+            send(connections[connection_marker], error_text.c_str(), size_of_error_text, NULL);
+            log(error_text, sent);
+        }
+        delete[] command;
+    }
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    WSAData wsadata;
+    WORD DLLVersion = MAKEWORD(2, 1); //запрашиваемая версия библиотеки винсок
+    if (WSAStartup(DLLVersion, &wsadata) != 0)
+    {
+        cout << "WSAStartup couldn't upload winsock version" << endl;
+        log("WSAStartup couldn't upload winsock version", " error: ");
+        exit(1);
+    }
+
+    SOCKADDR_IN addr;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(1027);
+    addr.sin_family = AF_INET; //константа для интернет протоколов
+
+    SOCKET Listen = socket(AF_INET, SOCK_STREAM, NULL);// coкстрим это протокол устанавливающий соединение
+    if (bind(Listen, (SOCKADDR*)&addr, sizeof(addr)) < 0)//привязка адресса сокету
+    {
+        cout << "Couldn't bind an addr" << endl;
+        log("Couldn't bind an addr", " error: ");
+        exit(1);
+    }
+
+    listen(Listen, SOMAXCONN);/*прослушиваем порт, сомаксконн это константа максимального значения
+                                клиентов, стоящих в очереди*/
+
+    while (true)
+    {
+        int addrsize = sizeof(addr);
+        SOCKET connection = accept(Listen, (SOCKADDR*)&addr, &addrsize);
+        if (connection == 0)
+        {
+            cout << "Couldn't connect with client" << endl;
+            log("Couldn't connect with client", " error: ");
+            return 1;
+        }
+        else
+        {
+            connections.push_back(connection);
+            connections_counter++;
+
+            cout << "Client connection accepted" << endl;
+            log("Client connection accepted", " uploaded successful: ");
+            char msg[256];
+            recv(connection, msg, sizeof(msg), NULL);
+            else
+                CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)SendInfo, (LPVOID)(connections_counter), NULL, NULL);
+        }
+    }
+    return 0;
+}
